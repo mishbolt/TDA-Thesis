42063c5e871d30f6168f432d5a7461c288c26781
mishbolt <95321921+mishbolt@users.noreply.github.com>
2023-01-10 10:00:04 -0700

    Add files via upload

---
+++ lorentz.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 29 19:09:19 2021
+
+@author: Misha
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from mpl_toolkits.mplot3d import Axes3D
+
+rho = 28.0
+sigma = 10.0
+beta = 8.0 / 3.0
+
+def f(state, t):
+    x, y, z = state  # Unpack the state vector
+    return sigma * (y - x), x * (rho - z) - y, x * y - beta * z  # Derivatives
+
+state0 = [0.01, 0.01, 0.01]
+t = np.arange(0.0, 40.0, 0.01)
+
+states = odeint(f, state0, t)
+
+fig = plt.figure()
+ax = fig.gca(projection="3d")
+ax.plot(states[:, 0], states[:, 1], states[:, 2])
+plt.draw()
+plt.show()
